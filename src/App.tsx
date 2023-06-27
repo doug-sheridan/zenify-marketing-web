9bb2ed14c468110211d21cce4e837ab43b952ecb
Doug Sheridan <dsherida@asu.edu>
2023-06-27 08:36:08 -0700

    add basic contact info and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2>
           ZENIFY | Marketing Made Easy
-        </p>
+        </h2>
+        <p>Get in contact with us now to learn how we can increase your marketing outreach, social media presence, and customer awareness.</p>
+        <a href={"mailto:admin@mystori.app"} className={'App-link'}>admin@mystori.app</a>
+        <a href={"tel:+14802782915"} className={'App-link'}>(480) 278-2915</a>
       </header>
     </div>
   );
